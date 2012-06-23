2f58b3262fd430c3ec19e9d22db15417ceb58fd5
John Shearer <jshearer@wevelgem.local>
2012-06-23 18:01:48 -0400

    Add an index.html page

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-#   twiteur.js
-### A super mega-awesome, vaguely Gallic twitter api plugin for jQuery.
+twiteur.js
+----------
+
+A vaguely Gallic twitter api plugin for jQuery.
+===============================================
 
 Or something.
--- jquery.twiteur.js
+++ jquery.twiteur.js
@@ -1,37 +1,38 @@
 /**
  * jquery.twiteur.js
- * 
+ *
  * bits and pieces -- indeed whole swathes -- shamelessly swiped from:
  *   https://github.com/seaofclouds/tweet (jquery.tweet.js)
  *   http://lupomontero.e-noise.com/blog/fetching-tweets-with-jquery-and-the-twitter-json-api
  */
 
-(function ($) {
-	$.fn.twiteur = function (obj) {
-		var i,
-		    api_url     = 'http://search.twitter.com/search.json',
-		    search_url  = 'http://search.twitter.com/',
-		    twitter_url = 'http://twitter.com/'
-		    s = $.extend({
-					query:       null,
-					user:        null,
-					hashtag:     null,
-					count:       1,
-					target:      this,
-					result_type: 'recent',
-					loading:     'Loading tweets...',
-					json:        true
-				}, obj)
-				regex = {
-					// See http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-					url: /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
-					hashtag:   /(?:^| )[#|\uFF03]+([\w\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u00ff\u0600-\u06ff]+)/gi,
-					user: /@(\w+)/gi
-				};
+(function ($, window, document, undefined) {
+	var pluginName = 'twiteur',
+	    defaults = {
+	    	query:       null,
+				user:        null,
+				hashtag:     null,
+				count:       1,
+				result_type: 'recent',
+				loading:     'Loading tweets...',
+				json:        true
+	    },
+	    api_url     = 'http://search.twitter.com/search.json',
+	    search_url  = 'http://search.twitter.com/',
+	    twitter_url = 'http://twitter.com/',
+			regex = {
+				// http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+				url: /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
+				hashtag:   /(?:^| )[#|\uFF03]+([\w\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u00ff\u0600-\u06ff]+)/gi,
+				user: /@(\w+)/gi
+			};
+
+	$.fn.twiteur = function (options) {
+
+		var s = $.extend({}, defaults, options)
 
 		function getTweets() {
-			$.ajax(
-				{
+			$.ajax({
 					url: api_url + '?callback=?',
 					data: {
 						q: makeQuery(),
@@ -39,7 +40,7 @@
 						include_entities: true
 					},
 				dataType: 'json',
-				context: s.target,
+				context: this,
 				success: dumpHTML
 			});
 		};
@@ -88,10 +89,11 @@
       linkUser: replacer(regex.user, "@<a href=\"http://"+s.twitter_url+"/$1\">$1</a>"),
       linkHash: replacer(regex.hashtag, ' <a href="'+search_url+'/search?tag=$1&lang=all">#$1</a>')
     });
-		
+
 		return this.each(function () {
-			$(this).append('<div class="loading">'+s.loading+'</div>');
-			getTweets();
+			var $this = $(this);
+			$this.append('<div class="loading">'+s.loading+'</div>');
+			getTweets.call($this);
 		});
 	}
-})(jQuery);
+})(jQuery, window, document);
